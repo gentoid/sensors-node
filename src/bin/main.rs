496ae589eda22cab3c10dfcf6f347bf286dff5ae
Viktor Lazarev <taurus101v@gmail.com>
2026-01-14 01:09:37 +0100

    Read measirements on BME680

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,11 +7,16 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
-use defmt::info;
+use core::cell::RefCell;
+use core::time;
+
+use bme680::{Bme680, I2CAddress, IIRFilterSize, PowerMode, SettingsBuilder};
+use defmt::{error, info};
+use embedded_hal_bus::i2c::RefCellDevice;
 use esp_hal::clock::CpuClock;
-use esp_hal::main;
 use esp_hal::time::{Duration, Instant};
 use esp_hal::timer::timg::TimerGroup;
+use esp_hal::{i2c, main};
 use esp_radio::ble::controller::BleConnector;
 use {esp_backtrace as _, esp_println as _};
 
@@ -27,28 +32,125 @@ esp_bootloader_esp_idf::esp_app_desc!();
 )]
 #[main]
 fn main() -> ! {
+    info!("Starting up 1");
     // generator version: 1.2.0
 
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    info!("Starting up 2");
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::_80MHz);
+    info!("Starting up 3");
     let peripherals = esp_hal::init(config);
 
+    info!("Starting up 4");
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 73744);
+    info!("Starting up 5");
     // COEX needs more RAM - so we've added some more
     esp_alloc::heap_allocator!(size: 64 * 1024);
 
+    info!("Starting up 6");
     let timg0 = TimerGroup::new(peripherals.TIMG0);
+    info!("Starting up 7");
     esp_rtos::start(timg0.timer0);
+    info!("Starting up 8");
     let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
+    info!("Starting up 9");
     let (mut _wifi_controller, _interfaces) =
         esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
             .expect("Failed to initialize Wi-Fi controller");
+    info!("Starting up 10");
     let _connector = BleConnector::new(&radio_init, peripherals.BT, Default::default());
 
+    // let rmt = Rmt::new(peripherals.RMT, Rate::from_mhz(80)).unwrap();
+
+    // let io = Io::new(peripherals.IO_MUX);
+
+    info!("Setting up I2C");
+    let i2c = i2c::master::I2c::new(peripherals.I2C0, i2c::master::Config::default())
+        .unwrap()
+        .with_sda(peripherals.GPIO1)
+        .with_scl(peripherals.GPIO2);
+
+    let i2c = RefCell::new(i2c);
+
+    info!("Setting up BME680");
+    let mut delayer = esp_hal::delay::Delay::new();
+    let mut bme_dev = Bme680::init(
+        RefCellDevice::new(&i2c),
+        &mut delayer,
+        I2CAddress::Primary, /* 0x76 */
+    )
+    .map_err(|err| match err {
+        bme680::Error::I2C(err) => {
+            error!("BME init error: I2C");
+            match err {
+                i2c::master::Error::FifoExceeded => error!("  I2C error: FifoExceeded"),
+                i2c::master::Error::AcknowledgeCheckFailed(err) => {
+                    error!("  I2C error: AcknowledgeCheckFailed");
+                    match err {
+                        i2c::master::AcknowledgeCheckFailedReason::Address => error!("    Address"),
+                        i2c::master::AcknowledgeCheckFailedReason::Data => error!("    Data"),
+                        i2c::master::AcknowledgeCheckFailedReason::Unknown => error!("    Unknown"),
+                        _ => error!("    ????"),
+                    }
+                },
+                i2c::master::Error::Timeout => error!("  I2C error: Timeout"),
+                i2c::master::Error::ArbitrationLost => error!("  I2C error: ArbitrationLost"),
+                i2c::master::Error::ExecutionIncomplete => error!("  I2C error: ExecutionIncomplete"),
+                i2c::master::Error::CommandNumberExceeded => error!("  I2C error: CommandNumberExceeded"),
+                i2c::master::Error::ZeroLengthInvalid => error!("  I2C error: ZeroLengthInvalid"),
+                i2c::master::Error::AddressInvalid(i2c_address) => error!("  I2C error: AddressInvalid: {}", i2c_address),
+                _ => todo!(),
+            };
+        },
+        bme680::Error::Delay => error!("BME init error: Delay"),
+        bme680::Error::DeviceNotFound => error!("BME init error: DeviceNotFound"),
+        bme680::Error::InvalidLength => error!("BME init error: InvalidLength"),
+        bme680::Error::DefinePwrMode => error!("BME init error: DefinePwrMode"),
+        bme680::Error::NoNewData => error!("BME init error: NoNewData"),
+        bme680::Error::BoundaryCheckFailure(_) => error!("BME init error: BoundaryCheckFailure"),
+    })
+    .unwrap();
+
+    info!("Setting up settings for BME680");
+    let settings = SettingsBuilder::new()
+        .with_temperature_oversampling(bme680::OversamplingSetting::OS2x)
+        .with_pressure_oversampling(bme680::OversamplingSetting::OS4x)
+        .with_humidity_oversampling(bme680::OversamplingSetting::OS2x)
+        .with_temperature_filter(IIRFilterSize::Size3)
+        .with_gas_measurement(time::Duration::from_millis(1500), 320, 25)
+        .with_temperature_offset(-2.2)
+        .with_run_gas(true)
+        .build();
+
+    let profile_dur = bme_dev.get_profile_dur(&settings.0).unwrap();
+    info!("Profile duration {:?}", profile_dur);
+    info!("Setting sensor settings");
+    bme_dev.set_sensor_settings(&mut delayer, settings).unwrap();
+
+    info!("Setting forced power modes");
+    bme_dev
+        .set_sensor_mode(&mut delayer, PowerMode::ForcedMode)
+        .unwrap();
+
+    // let sensor_settings = bme_dev.get_sensor_settings(settings.1);
+    // info!("Sensor settings: {}", sensor_settings);
+
     loop {
-        info!("Hello world!");
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
-    }
 
+        // let power_mode = bme_dev.get_sensor_mode();
+        // info!("Sensor power mode: {}", power_mode);
+        info!("Setting forced power modes");
+        bme_dev
+            .set_sensor_mode(&mut delayer, PowerMode::ForcedMode)
+            .unwrap();
+        info!("Retrieving sensor data");
+        let (data, _state) = bme_dev.get_sensor_data(&mut delayer).unwrap();
+        // info!("Sensor Data {:?}", data);
+        info!("Temperature {}°C", data.temperature_celsius());
+        info!("Pressure {}hPa", data.pressure_hpa());
+        info!("Humidity {}%", data.humidity_percent());
+        info!("Gas Resistance {}Ω", data.gas_resistance_ohm());
+    }
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
 }
